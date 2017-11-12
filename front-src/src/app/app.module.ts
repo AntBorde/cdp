2c567c95ea234418bf1f3b2f30fc666c9d9b6484
Shervin Sarain <velezsarain@gmail.com>
2017-11-12 22:52:26 +0100

    Travail en cours: création du front end en angular 5

--- front-src/src/app/app.module.ts
+++ front-src/src/app/app.module.ts
@@ -1,20 +1,26 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import { LoginFieldComponent } from './login-field/login-field.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { AccountCreationComponent } from './account-creation/account-creation.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginFieldComponent,
+    AccountCreationComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
