7c37d12868ecfdaf7d9ed8ef6357643d3779bcf0
AntBorde <antoine33.borde@gmail.com>
2017-11-13 16:43:13 +0100

    implementation GET user et project,  commencer le POST project

--- back-src/routes/index.js
+++ back-src/routes/index.js
@@ -5,18 +5,72 @@ let cors = require('cors');
 
 router.use(cors());
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
-  db.query('SELECT * from Project', function (error, results, fields) {
-    if (error)
-      throw error;
-    else
-      res.send(JSON.stringify(results));
-  });
+      res.send("ok");
 });
 
 router.get('/users' , function(req, res, next) {
   db.query('SELECT * from User',function (error, results, fields) {
+    if (error){
+      throw error;
+    }
+    else {
+      res.send(JSON.stringify(results));
+    }
+  });
+});
+
+/*router.post('/users' , function(req, res, next) {
+  db.query('SELECT * from User', function (error, results, fields) {
+    if (error){
+      throw error;
+    }
+    else {
+      res.send(JSON.stringify(results));
+    }
+  });
+});*/
+
+router.get('/users/:id' , function(req, res, next) {
+  db.query('SELECT * from User WHERE user_id ='+ req.params.id, function (error, results, fields) {
+    if (error){
+      throw error;
+    }
+    else {
+      res.send(JSON.stringify(results));
+    }
+  });
+});
+
+
+router.get('/project' , function(req, res, next) {
+  db.query('SELECT * from Project', function (error, results, fields) {
+    if (error){
+      throw error;
+    }
+    else {
+      res.send(JSON.stringify(results));
+    }
+  });
+});
+
+
+router.post('/project' , function(req, res, next) {
+  console.log("=================================================================================================================================================================================================================================");
+  console.log(req.body.Name);
+  /*db.query('INSERT INTO Project(name,describle,git) VALUES (req.body.name,req.body.describle,req.body.git)', function (error, results, fields) {
+    if (error){
+      throw error;
+    }
+    else {
+      res.send(JSON.stringify(results));
+    }
+  });*/
+});
+
+
+router.get('/project/:nom' , function(req, res, next) {
+  db.query('SELECT * from Project WHERE name =' + req.params.nom, function (error, results, fields) {
     if (error){
       throw error;
     }
