8c37ee2fd0f789977c0a5827aca481510dbecb5b
Mathieu Lirzin <mathieu.lirzin@etu.u-bordeaux.fr>
2017-11-16 17:29:06 +0100

    Déplacement des routes /users vers module dédié

--- back-src/routes/index.js
+++ back-src/routes/index.js
@@ -12,53 +12,6 @@ router.get('/', (req, res, next) => {
   res.send('Server live');
 });
 
-/* GET Users */
-router.get('/users', function(req, res, next) {
-  db.query('SELECT * from User',function (error, results, fields) {
-    if (error){
-      throw error;
-    }
-    else {
-      res.send(JSON.stringify(results));
-    }
-  });
-});
-
-//create user account
-router.post('/users', function(req, res, next) {
-  db.query('SELECT * from User where pseudo = ?',[req.body.pseudo], function(error, results, fields) {
-    if (error) {
-      throw error;
-    } else if (results.length != 0) {
-      console.log("user already exist");
-    } else {
-      db.query('INSERT INTO User(pseudo,name,first_name,birth_date,password) VALUES (?,?,?,?,?)',
-	       [req.body.pseudo, req.body.name, req.body.first_name, req.body.birth_day, req.body.password],
-	       function(error, results, fields) {
-                 if (error) {
-                   throw error;
-                 } else {
-                   res.send(JSON.stringify(results));
-                 }
-               });
-    }
-  });
-});
-
-
-/* GET User by id */
-router.get('/users/:id' , function(req, res, next) {
-  db.query('SELECT * from User WHERE user_id = ?',[req.params.id], function (error, results, fields) {
-    if (error){
-      throw error;
-    }
-    else {
-      res.send(JSON.stringify(results));
-    }
-  });
-});
-
-
 /*Connexion*/
 router.post('/login', (req, res, next) => {
   var username = req.body.pseudo;
--- back-src/routes/users.js
+++ back-src/routes/users.js
@@ -1,9 +1,49 @@
 let express = require('express');
+let db = require('../db');
 let router = express.Router();
 
-/* GET users listing. */
+/* GET Users */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  db.query('SELECT * from User',function (error, results, fields) {
+    if (error){
+      throw error;
+    }
+    else {
+      res.send(JSON.stringify(results));
+    }
+  });
+});
+
+//create user account
+router.post('/', function(req, res, next) {
+  db.query('SELECT * from User where pseudo = ?', [req.body.pseudo], function(error, results, fields) {
+    if (error) {
+      throw error;
+    } else if (results.length != 0) {
+      console.log("user already exist");
+    } else {
+      db.query('INSERT INTO User(pseudo,name,first_name,birth_date,password) VALUES (?,?,?,?,?)',
+	       [req.body.pseudo, req.body.name, req.body.first_name, req.body.birth_day, req.body.password],
+	       function(error, results, fields) {
+                 if (error) {
+                   throw error;
+                 } else {
+                   res.send(JSON.stringify(results));
+                 }
+               });
+    }
+  });
+});
+
+/* GET User by id */
+router.get('/:id' , function(req, res, next) {
+  db.query('SELECT * from User WHERE user_id = ?',[req.params.id], function (error, results, fields) {
+    if (error){
+      throw error;
+    } else {
+      res.send(JSON.stringify(results));
+    }
+  });
 });
 
 module.exports = router;
