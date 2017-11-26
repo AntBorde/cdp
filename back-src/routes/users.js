125831c2b4b7e0844337573eef7c6c0deac9edcb
ygadi <gadiyounes1994@gmail.com>
2017-11-26 23:43:19 +0100

    Modification de route users vers sequelize

--- back-src/routes/users.js
+++ back-src/routes/users.js
@@ -1,48 +1,79 @@
 let express = require('express');
 let router = express.Router();
+var models  = require('../models');
 
 /* GET Users */
 router.get('/', function(req, res, next) {
-  db.query('SELECT * from User',function (error, results, fields) {
-    if (error){
-      throw error;
-    }
-    else {
-      res.send(JSON.stringify(results));
-    }
-  });
+  models.users.findAll().
+  then(users=>{
+  if(users==null)
+  res.send("users not found");
+  else
+  res.send(users);
+  }).catch(err=> {res.send(err)})
 });
 
-//Create user account
-router.post('/', function(req, res, next) {
-  db.query('SELECT * from User where pseudo = ?', [req.body.pseudo], function(error, results, fields) {
-    if (error) {
-      throw error;
-    } else if (results.length != 0) {
-      console.log("user already exist");
-    } else {
-      db.query('INSERT INTO User(email,name,birth_date,password) VALUES (?,?,?,?)',
-	       [req.body.email, req.body.name, req.body.first_name, req.body.birth_day, req.body.password],
-	       function(error, results, fields) {
-                 if (error) {
-                   throw error;
-                 } else {
-                   res.send(JSON.stringify(results));
-                 }
-               });
-    }
-  });
-});
-
-/* GET User by id */
+/*GET User by id*/
 router.get('/:id' , function(req, res, next) {
-  db.query('SELECT * from User WHERE user_id = ?',[req.params.id], function (error, results, fields) {
-    if (error){
-      throw error;
-    } else {
-      res.send(JSON.stringify(results));
-    }
-  });
+  models.users.findById(req.params.id).
+  then(user=>{
+  if(user==null)
+  res.send("user not found");
+  else
+  res.send(user);
+  }).catch(err=> {res.send(err)})
+});
+
+
+/*Create user account*/
+router.post('/', function(req, res, next) {
+ models.users.findOne({where: {email:req.body.email}}).
+ then(user=>{
+ if(user!=null)
+ res.send("user already exist with the same email");
+ else
+ {
+   models.users.create({email:req.body.email,firstname:req.body.firstname,lastname:req.body.lastname,password:req.body.password,birth_date:req.body.birth_date}).
+   then(ress=>{
+     res.send("user created");
+   }).catch(err=> {res.send(err)})
+ }
+ }).catch(err=> {res.send(err)})
+});
+
+
+/** PUT Modifie utilisateur*/
+router.put('/:id' , function(req, res, next) {
+models.users.findById(req.params.id).
+then(user=>{
+if(user==null)
+res.send("user not found");
+else
+{
+  models.users.update(
+  {email:req.body.email,password:req.body.password},
+  { where: { user_id:req.params.id}}).
+  then(ress=>{
+    res.send("user updated");
+  }).catch(err=> {res.send(err)})
+}
+}).catch(err=> {res.send(err)})
+});
+/* Delete utilisateur*/
+router.delete('/:id' , function(req, res, next) {
+models.users.findById(req.params.id).
+then(user=>{
+if(user==null)
+res.send("user not found");
+else
+{
+  models.users.destroy(
+  { where: {user_id :req.params.id}}).
+  then(ress=>{
+    res.send("user deleted");
+  }).catch(err=> {res.send(err)})
+}
+}).catch(err=> {res.send(err)})
 });
 
 module.exports = router;
