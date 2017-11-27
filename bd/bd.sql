7e0efc41e9fc7837f26fff1eca921f2f42f4b968
ygadi <gadiyounes1994@gmail.com>
2017-11-27 09:50:17 +0100

    supression de la table SprintTask
    Rajouter sprint_id dans la table task comme clé étrangere

--- bd/bd.sql
+++ bd/bd.sql
@@ -18,9 +18,7 @@ create table if not exists User
     birth_date    date,
     primary key(user_id)
   );
-
-
-  -- =================================================================
+ -- =================================================================
   --      table : Project
   -- =================================================================
   create table if not exists Project
@@ -33,7 +31,7 @@ create table if not exists User
 
 
   -- =================================================================
-  --      table : Project
+  --      table : Project_Team
   -- =================================================================
 create table if not exists Project_team
   (
@@ -43,8 +41,8 @@ create table if not exists Project_team
     primary key (name,user_id),
     constraint project_member
           foreign key (name)
-          references Project(name),
-          ON DELETE CASCADE
+          references Project(name)
+          ON DELETE CASCADE,
     constraint member
           foreign key (user_id)
           references User(user_id)
@@ -70,28 +68,6 @@ create table if not exists Issue
           ON DELETE CASCADE
   );
 
-
-  -- =================================================================
-  --      table : Tache
-  -- =================================================================
-create table if not exists Task
-  (
-    task_id       integer        not null       auto_increment,
-    describle     Text           not null,
-    user_id       integer,
-    state         varchar(10)    not null,
-    cost          integer,
-    name          varchar(125)    not null,
-    primary key (task_id),
-    constraint project_task
-          foreign key (name)
-          references Project(name) 
-           ON DELETE CASCADE,
-    constraint user_task
-          foreign key (user_id)
-          references User(user_id)
-  );
-
   -- =================================================================
   --      table : Sprint
   -- =================================================================
@@ -107,25 +83,32 @@ create table if not exists Task
             foreign key (name)
             references Project(name) ON DELETE CASCADE
     );
+       -- =================================================================
+  --      table : Tache
+  -- =================================================================
+create table if not exists Task
+  (
+    task_id       integer        not null       auto_increment,
+    describle     Text           not null,
+    user_id       integer,
+    state         varchar(10)    not null,
+    cost          integer,
+    name          varchar(125)    not null,
+    sprint_id     integer        not null ,
+    primary key (task_id),
+    constraint project_task
+          foreign key (name)
+          references Project(name) 
+           ON DELETE CASCADE,
+    constraint user_task
+          foreign key (user_id)
+          references User(user_id),
+    constraint sprint_task
+          foreign key (sprint_id)
+          references Sprint(sprint_id) ON DELETE CASCADE 
+  );
 
-  -- =================================================================
-  --      table : Sprint_Task
-  -- =================================================================
-  create table if not exists SprintTask
-    (
-      sprintTask_id   integer      not null     auto_increment,
-      sprint_id       integer      not null,
-      task_id         integer      not null,
-      primary key (sprintTask_id),
-      constraint task_sprint
-            foreign key (task_id)
-            references Task(task_id) ON DELETE CASCADE,
-      constraint sprint
-            foreign key (sprint_id)
-            references Sprint(sprint_id)ON DELETE CASCADE
-    );
-
-  -- =================================================================
+ -- =================================================================
   --      table : Build
   -- =================================================================
   create table if not exists Build
@@ -138,13 +121,7 @@ create table if not exists Task
             foreign key (name)
             references Project(name) ON DELETE CASCADE
     );
-
-
-
-
-
-
-    -- =================================================================
+  -- =================================================================
     -- =================================================================
     --      Ajout de données
     -- =================================================================
@@ -176,21 +153,14 @@ create table if not exists Task
     ('je veux une belle view',2,2,'TODO','cdp'),
     ('je veux la présence d\'une bd',2,2,'TODO','cdp');
 
-  insert into Task (describle,user_id,state,name)
-    values
-    ('faire un truc',1,'TODO','web'),
-    ('faire un autre truc',2,'TODO','web'),
-    ('faire la doc',3,'TODO','cdp'),
-    ('faire les builds',4,'TODO','cdp');
-
   insert into Sprint (describle,name)
     values
     ('arrivé a faire tout ça','web'),
     ('arrivé a faire tout ça','cdp');
 
-  insert into SprintTask (sprint_id,task_id)
+   insert into Task (describle,user_id,state,name,sprint_id)
     values
-    (1,1),
-    (1,2),
-    (2,3),
-    (2,4);
+    ('faire un truc',1,'TODO','web',1),
+    ('faire un autre truc',2,'TODO','web',1),
+    ('faire la doc',3,'TODO','cdp',2),
+    ('faire les builds',4,'TODO','cdp',2);
