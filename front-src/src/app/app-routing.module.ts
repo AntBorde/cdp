9a2360d9fbe596c0ffedd28c181d075043bf5777
ysgadi <gadiyounes1994@gmail.com>
2017-12-05 17:06:21 +0100

    Mettre en place le système de protection des routes

--- front-src/src/app/app-routing.module.ts
+++ front-src/src/app/app-routing.module.ts
@@ -6,14 +6,15 @@ import { SigninComponent } from './components/signin/signin.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { UpdateProfileComponent } from './components/update-profile/update-profile.component';
+import { AuthGuard } from './guard/authentification.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent},
   { path: 'signin', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'profile', component: ProfileComponent},
-  { path: 'Updateprofile', component: UpdateProfileComponent},
+  { path: 'profile', component: ProfileComponent,canActivate:[AuthGuard]},
+  { path: 'Updateprofile', component: UpdateProfileComponent,canActivate:[AuthGuard]},
 ];
 
 @NgModule({
@@ -22,6 +23,7 @@ const routes: Routes = [
   ],
   exports: [
     RouterModule
-  ]
+  ],
+  providers: [AuthGuard]
 })
 export class AppRoutingModule { }
