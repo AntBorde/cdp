eb7cc4fffa6ad1c54b0f2b803c690939bf3c1c52
AntBorde <antoine33.borde@gmail.com>
2017-12-08 14:15:32 +0100

    correction du test d'inscription il est désormais fonctionnel

--- tests/e2e/registerSpec.js
+++ tests/e2e/registerSpec.js
@@ -1,31 +1,26 @@
 describe('inscription', function() {
 
-  var rdm = Math.random();
-  var mail = 'test'+rdm+'@gmail.com';
-
-  beforeEach(function() {
+  beforeEach(function(){
     browser.get('http://localhost:4200/home');
   });
-
-
   it('tentative de register', function() {
     var registerButton = element(by.id('registerBtn'));
     registerButton.click();
-
     var email = element(by.id('inputEmail'));
     var firstName = element(by.id('inputFirstName'));
     var lastName = element(by.id('inputLastName'));
     var password = element(by.id('inputPassword'));
     var password2 = element(by.id('inputPassword2'));
 
-    email.sendKeys(mail);
+    email.sendKeys('toto@gmail.com');
     firstName.sendKeys('toto');
     lastName.sendKeys('toto1');
     password.sendKeys('blablabla');
-    password.sendKeys('blablabla');
+    password2.sendKeys('blablabla');
 
     var saveButton = element(by.id('createUser'));
     saveButton.click();
+
   });
 
 
@@ -36,11 +31,13 @@ describe('inscription', function() {
     var email = element(by.id('inputEmail'));
     var password = element(by.id('inputPassword'));
 
-    email.sendKeys(mail);
+    email.sendKeys('toto@gmail.com');
+    browser.pause();
     password.sendKeys('blablabla');
 
     loginButton = element(by.id('login'));
     loginButton.click();
   });
+
 
 });
