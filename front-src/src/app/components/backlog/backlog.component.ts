556c19b99a3bf0fd8e49ad6b976c3459d9ef8222
ysgadi <gadiyounes1994@gmail.com>
2017-12-11 22:06:54 +0100

    Mettre en place du composant backlog

--- front-src/src/app/components/backlog/backlog.component.ts
+++ front-src/src/app/components/backlog/backlog.component.ts
@@ -3,8 +3,8 @@ import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms'
 import { CustomValidators } from 'ng2-validation';
 import { HttpClient, HttpErrorResponse,HttpHeaders} from '@angular/common/http';
 import {Router} from '@angular/router';
-import {Issue} from '../../models/issue';
 import { AuthService } from "../../services/auth.service";
+import {Issue} from '../../models/issue';
 import {ProjectService} from "../../services/project.service";
 import {ActivatedRoute} from '@angular/router';
 
@@ -17,18 +17,30 @@ import {ActivatedRoute} from '@angular/router';
 export class BacklogComponent implements OnInit {
   projectName:string;
   ProductOwner: string;
-  Backlog:Issue[]=null;
+  Backlog:Issue[]=null
   constructor(private http: HttpClient,
     private activatedRoute: ActivatedRoute) { }
    projectId=this.activatedRoute.snapshot.paramMap.get('idProject');
   ngOnInit() {
     this.http
-    .get<Issue[]>('http://localhost:3000/api/projects/'+this.projectId+'/issues')
+    .get<ProductOwnerResponse>('http://localhost:3000/api/projects/'+this.projectId+'/productOwner')
     .subscribe(
       data => {
-        this.Backlog=data;
-        console.log(this.Backlog);
+      this.projectName=data.projectName;
+      this.ProductOwner=data.productOwner;
       }
     );
+    this.
+    http.
+    get<Issue[]>('http://localhost:3000/api/projects/'+this.projectId+'/issues')
+    .subscribe( data => { this.Backlog=data; console.log(this.Backlog);
+     });
+
   }
+}
+
+interface ProductOwnerResponse
+{
+projectName:string;
+productOwner:string;
 }
